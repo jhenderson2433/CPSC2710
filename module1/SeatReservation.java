e659af6b4cbf5eb7f734e8da0dcd1bdbd63b3542
Tre Henderson CPSC 2710 <Jhenderson2433@gmail.com>
2026-01-13 20:07:13 -0600

    add private instnace varibles

--- module1/SeatReservation.java
+++ module1/SeatReservation.java
@@ -1,2 +1,10 @@
+import java.time.LocalDate;
+
 public class SeatReservation {
+
+   private String flightDesignator;
+   private LocalDate flightDate;
+   private String firstName;
+   private String lastName;
+
 }
