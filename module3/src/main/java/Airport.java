8cee13046977968143ea871eca25e125c9af0216
Tre Henderson CPSC 2710 <Jhenderson2433@gmail.com>
2026-01-28 20:29:19 -0600

    Add Latitude and Longitude and gernerate getters and setters

--- module3/src/main/java/Airport.java
+++ module3/src/main/java/Airport.java
@@ -37,6 +37,110 @@ public class Airport {
     // Optional: empty constructor (useful for JavaFX / frameworks)
     public Airport() { }
 
+    public String getIdent() {
+        return ident;
+    }
+
+    public void setIdent(String ident) {
+        this.ident = ident;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getElevationFt() {
+        return elevationFt;
+    }
+
+    public void setElevationFt(Integer elevationFt) {
+        this.elevationFt = elevationFt;
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+
+    public String getIsoCountry() {
+        return isoCountry;
+    }
+
+    public void setIsoCountry(String isoCountry) {
+        this.isoCountry = isoCountry;
+    }
+
+    public String getIsoRegion() {
+        return isoRegion;
+    }
+
+    public void setIsoRegion(String isoRegion) {
+        this.isoRegion = isoRegion;
+    }
+
+    public String getMunicipality() {
+        return municipality;
+    }
+
+    public void setMunicipality(String municipality) {
+        this.municipality = municipality;
+    }
+
+    public String getGpsCode() {
+        return gpsCode;
+    }
+
+    public void setGpsCode(String gpsCode) {
+        this.gpsCode = gpsCode;
+    }
+
+    public String getIataCode() {
+        return iataCode;
+    }
+
+    public void setIataCode(String iataCode) {
+        this.iataCode = iataCode;
+    }
+
+    public String getLocalCode() {
+        return localCode;
+    }
+
+    public void setLocalCode(String localCode) {
+        this.localCode = localCode;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
     // Optional: constructor you can use internally
     public Airport(String ident, String type, String name, Integer elevationFt,
                    String continent, String isoCountry, String isoRegion,
